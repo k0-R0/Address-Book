897ad02fae9003e1ae2e4eca610f1bedeaf540b4
k0-R0 <bmenonprayush@gmail.com>
2026-02-26 06:14:05 +0530

    Add file error handling

--- file.c
+++ file.c
@@ -4,6 +4,11 @@
 
 void saveContactsToFile(AddressBook *addressBook) {
     FILE *contactsFile = fopen("contacts.txt", "w+");
+    if (!contactsFile) {
+        fprintf(stderr, "File not found\n");
+        return;
+    }
+
     for (int i = 0; i < addressBook->contactCount; i++) {
         fprintf(contactsFile, "%s,%s,%s\n", addressBook->contacts[i].name,
                 addressBook->contacts[i].phone, addressBook->contacts[i].email);
@@ -20,14 +25,16 @@ void loadContactsFromFile(AddressBook *addressBook) {
 
     char lineBuffer[120];
 
-    while (fscanf(contactsFile, "%[^\n]", lineBuffer) == 1) {
+    while (fscanf(contactsFile, "%[^\n]", lineBuffer) > 0) {
 
         char *name = strtok(lineBuffer, ",");
         char *phone = strtok(NULL, ",");
         char *email = strtok(NULL, ",");
 
-        if (!name || !phone || !email)
+        if (!name || !phone || !email) {
+            printf("File not formatted correctly\n");
             continue; // skip malformed lines
+        }
 
         strcpy(addressBook->contacts[addressBook->contactCount].name, name);
         strcpy(addressBook->contacts[addressBook->contactCount].phone, phone);
