430b8a95e6bcc784a4f19804cb3456b2aef4100c
k0-R0 <bmenonprayush@gmail.com>
2026-02-26 16:28:48 +0530

    Add documentation for contacts and file

--- contact.c
+++ contact.c
@@ -1,25 +1,30 @@
 #include "contact.h"
-// #include "file.h"
+#include "file.h"
 #include "populate.h"
 #include "renderTable.h"
 #include "sortEngine.h"
 #include "validations.h"
 #include <ctype.h>
 #include <stdio.h>
-// #include <stdlib.h>
 #include <stdlib.h>
 #include <string.h>
 
+// function to validate proper integers are being given when asked for options
 int readInt(int *value) {
+    // create a string buffer and store to it first
     char buffer[20];
     scanf("%s", buffer);
+    // ensure that the string only has numbers
     for (int i = 0; buffer[i]; i++) {
         if (!isdigit(buffer[i])) {
+            // clear the buffer if alphabets or symbols are present so
+            // that the next input from user is read
             while (getchar() != '\n')
                 ;
             return 0;
         }
     }
+    // convert the string to int if there were only numerics and return 1
     *value = atoi(buffer);
     return 1;
 }
@@ -54,16 +59,13 @@ void listContacts(AddressBook *addressBook) {
 
 void initialize(AddressBook *addressBook) {
     addressBook->contactCount = 0;
-    populateAddressBook(addressBook);
-
-    // Load contacts from file during initialization (After files)
-    // loadContactsFromFile(addressBook);
+    loadContactsFromFile(addressBook);
 }
 
-// void saveAndExit(AddressBook *addressBook) {
-//     saveContactsToFile(addressBook); // Save contacts to file
-//     exit(EXIT_SUCCESS); // Exit the program
-// }
+void saveAndExit(AddressBook *addressBook) {
+    saveContactsToFile(addressBook); // Save contacts to file
+    exit(EXIT_SUCCESS);              // Exit the program
+}
 
 void createContact(AddressBook *addressBook) {
     /* Define the logic to create a Contacts */
@@ -73,6 +75,7 @@ void createContact(AddressBook *addressBook) {
     printf("Enter name of contact : \n");
     scanf(" %49[^\n]", name);
 
+    // keep asking for input till a valid number is entered
     do {
         printf("Enter phone of contact : \n");
         scanf("%s", phone);
@@ -80,6 +83,7 @@ void createContact(AddressBook *addressBook) {
             printf("Invalid or duplicate phone number. Try again.\n");
     } while (!validatePhone(addressBook, phone));
 
+    // keep asking for input till a valid email is entered
     do {
         printf("Enter email of contact : \n");
         scanf("%s", email);
@@ -101,6 +105,7 @@ int searchContact(AddressBook *addressBook, int *arr) {
     /* Define the logic for search */
     printf("Search by one of the following fields:\n1. Name\n2. "
            "Phone\n3. Email\n");
+    // keep asking for user input to filter on till valid filter is given
     int searchFilter;
     do {
         if (!readInt(&searchFilter)) {
@@ -117,6 +122,8 @@ int searchContact(AddressBook *addressBook, int *arr) {
     printf("Enter search text...\n");
     char searchText[50];
     char contactPresent = 0;
+    // if searching by phone verify only numerics are there in the
+    // search text otherwise we can exit early
     if (searchFilter == PHONE) {
         do {
             scanf("%s", searchText);
@@ -131,6 +138,9 @@ int searchContact(AddressBook *addressBook, int *arr) {
         scanf("%s", searchText);
     }
     char *contactInfo;
+    // based on the filter provided look for the string in a different field
+    // using enums to represent the different options
+    // reference can be found in sortEngine
     for (int i = 0; i < addressBook->contactCount; i++) {
         const char *field;
         switch (searchFilter) {
@@ -144,6 +154,8 @@ int searchContact(AddressBook *addressBook, int *arr) {
             field = addressBook->contacts[i].email;
             break;
         }
+        // doing a case insensitive search as user can
+        // enter lower case letters for search
         if (strcasestr(field, searchText)) {
             if (!contactPresent) {
                 printTableHeaders();
@@ -158,6 +170,7 @@ int searchContact(AddressBook *addressBook, int *arr) {
         printMessage("Contact not found.");
     return contactPresent;
 }
+
 void editContactName(AddressBook *addressBook, int index) {
     char name[50];
     printf("Enter name of contact : \n");
@@ -189,6 +202,8 @@ void editContactEmail(AddressBook *addressBook, int index) {
 
 void editContact(AddressBook *addressBook) {
     /* Define the logic for Editcontact */
+    // use this arr array to keep track of search results
+    // arr will have contact index set to 1 if contact is present during search
     int arr[100] = {0};
     int contactPresent = searchContact(addressBook, arr);
     if (!contactPresent)
@@ -197,10 +212,13 @@ void editContact(AddressBook *addressBook) {
 
     int index;
     do {
+        // check if user entered a proper number
         if (!readInt(&index)) {
             printf("Please enter a number.\n");
             continue;
         }
+        // check if index is out of range or if it is not returned from the
+        // search
         if (index < 1 || index > addressBook->contactCount || !arr[index - 1]) {
             printf("Please select one of the contacts listed above\n");
             continue;
@@ -223,6 +241,7 @@ void editContact(AddressBook *addressBook) {
     } while (choice < 1 || choice > 4);
 
     ContactInfo sc = (ContactInfo)choice;
+    // enums for different contact info , refer sortEngine.c
     switch (sc) {
     case NAME:
         editContactName(addressBook, index - 1);
@@ -245,6 +264,8 @@ void editContact(AddressBook *addressBook) {
 
 void deleteContact(AddressBook *addressBook) {
     /* Define the logic for deletecontact */
+    // use this arr array to keep track of search results
+    // arr will have contact index set to 1 if contact is present during search
     int arr[100] = {0};
     int contactPresent = searchContact(addressBook, arr);
     if (!contactPresent)
@@ -257,6 +278,8 @@ void deleteContact(AddressBook *addressBook) {
             printf("Please enter a number.\n");
             continue;
         }
+        // check if index is out of range or if it is not returned from the
+        // search
         if (index < 1 || index > addressBook->contactCount || !arr[index - 1]) {
             printf("Please select one of the contacts listed above\n");
             continue;
--- file.c
+++ file.c
@@ -4,35 +4,40 @@
 #include <string.h>
 
 void saveContactsToFile(AddressBook *addressBook) {
-    FILE *contactsFile = fopen("contacts.txt", "w+");
+    // open file
+    FILE *contactsFile = fopen("contacts.csv", "w+");
     if (!contactsFile) {
         fprintf(stderr, "File not found\n");
         return;
     }
-
+    // print all contacts in a formatted manner
     for (int i = 0; i < addressBook->contactCount; i++) {
         fprintf(contactsFile, "%s,%s,%s\n", addressBook->contacts[i].name,
                 addressBook->contacts[i].phone, addressBook->contacts[i].email);
     }
     fclose(contactsFile);
+    // early exit once user saves
     exit(0);
 }
 
 void loadContactsFromFile(AddressBook *addressBook) {
-    FILE *contactsFile = fopen("contacts.txt", "r");
+    FILE *contactsFile = fopen("contacts.csv", "r");
     if (!contactsFile) {
         fprintf(stderr, "File not found\n");
         return;
     }
 
+    // lineBuffer to read each line from the file
     char lineBuffer[120];
 
     while (fscanf(contactsFile, "%[^\n]", lineBuffer) > 0) {
 
+        // strtok to get each string before the delimiter ','
         char *name = strtok(lineBuffer, ",");
         char *phone = strtok(NULL, ",");
         char *email = strtok(NULL, ",");
-
+        // check for corrupted file inform the user and continue reading rest of
+        // the data
         if (!name || !phone || !email) {
             printf("File not formatted correctly\n");
             continue; // skip malformed lines
@@ -43,6 +48,8 @@ void loadContactsFromFile(AddressBook *addressBook) {
         strcpy(addressBook->contacts[addressBook->contactCount].email, email);
 
         addressBook->contactCount++;
+        // because we're using scanf to read each line '\n' is left in stdin
+        // buffer clear it before reading next line
         fgetc(contactsFile);
     }
 
